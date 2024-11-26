7a9921afef964f66caf76dd1bcfccb6b45c43c72
ChilledPi <chilledpi7@gmail.com>
2024-11-27 02:27:40 +0900

    feat: Add init

---
+++ src/main/java/com/example/tickethub_producer/TickethubProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tickethub_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickethubProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickethubProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tickethub_producer/TickethubProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tickethub_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickethubProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
